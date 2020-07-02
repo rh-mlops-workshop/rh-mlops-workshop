56c3679e37bd7850238a066303664dfa00cd401d
Tsai Li Ming <tsai.liming@gmail.com>
2020-07-02 18:14:16 +0800

    update path

--- src/train/train-stage.sh
+++ src/train/train-stage.sh
@@ -7,7 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . $DIR/config.sh
 
 pip install -U pip setuptools
-pip install -r /workspace/source/workshop/src/train/requirements.txt
+pip install -r $DIR/requirements.txt
 
 echo "Running training"
 python $DIR/$PYTHON_SCRIPT -m /workspace/model -r $RUN_NAME
