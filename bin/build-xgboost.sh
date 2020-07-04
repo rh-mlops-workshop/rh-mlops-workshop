8bac4169da86585557693125da49fc1a58e3502c
Tsai Li Ming <tsai.liming@gmail.com>
2020-07-04 23:21:06 +0800

    remove dask

--- bin/build-xgboost.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp /projects/datascience-examples/workshop/src/train/boost.ans.py \
-    /projects/datascience-examples/workshop/src/train/boost.py
-
-
-
-
--- src/train/train-dev.sh
+++ src/train/train-dev.sh
@@ -3,6 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . $DIR/config.sh
 
+pip uninstall -y dask
 pip install -U pip setuptools grpcio-tools==1.29.0 seldon-core==1.1.0 
 pip install -r $DIR/requirements.txt
 
